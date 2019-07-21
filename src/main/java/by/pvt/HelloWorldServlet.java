fa811ae5c7efbb563729e848591974ba6abb8b21
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2019-07-21 13:29:29 +0300

    updates pom.xml

--- src/main/java/by/pvt/HelloWorldServlet.java
+++ src/main/java/by/pvt/HelloWorldServlet.java
@@ -1,6 +1,9 @@
 package by.pvt;
 
+import by.pvt.dto.SystemUsers;
+
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,10 +13,19 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(value="/helloWorld", name="helloWorldServlet")
 public class HelloWorldServlet extends HttpServlet {
 
+    private SystemUsersService systemUsersService;
+
+    @Override
+    public void init() throws ServletException {
+        systemUsersService = new SystemUsersService();
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setStatus(200);
         resp.setHeader("Content-Type", "application/json");
+        List<SystemUsers> users = systemUsersService.getSystemUsers();
+        System.out.println("User size= " + users.size());
         resp.getWriter().print("{\"message\": \"hello, world!\"}");
     }
 
